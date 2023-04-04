4b6e37ef62e175008ef1b09e87b0a5a945026e1c
pramitsingh0 <pramitsingh0@gmail.com>
2023-04-05 01:20:53 +0530

    2.9 phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,65 @@
 import { useState } from "react";
 const Header = () => <h2>Phonebook</h2>;
+const Input = ({ text, value, changeHandle }) => {
+  return (
+    <div>
+      {text}: <input value={value} onChange={changeHandle} />
+    </div>
+  );
+};
+const SubmitBtn = () => (
+  <div>
+    <button type="submit">Add</button>
+  </div>
+);
+const ContactForm = ({
+  submitHandler,
+  nameChangeHandle,
+  numberChangeHandle,
+  name,
+  number,
+}) => {
+  return (
+    <form onSubmit={submitHandler}>
+      <Input text="name" value={name} changeHandle={nameChangeHandle} />
+      <Input text="number" value={number} changeHandle={numberChangeHandle} />
+      <SubmitBtn />
+    </form>
+  );
+};
+const Display = ({ persons, filter }) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        if (person.name.toLowerCase().includes(filter.toLowerCase())) {
+        return <p key={person.name}>{person.name} {person.number}</p>
+        }
+      })
+      }
+    </div>
+  );
+};
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
   const nameChangeHandler = (e) => {
     setNewName(e.target.value);
   };
   const numberChangeHandler = (e) => {
     setNewNumber(e.target.value);
-  }
+  };
+  const filterChangeHandler = (e) => {
+    setFilter(e.target.value);
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    const dupFound = persons.find(person => person.name === newName);
+    const dupFound = persons.find((person) => person.name === newName);
     if (dupFound) {
       return alert(`${newName} already added to the phonebook`);
     }
@@ -23,22 +70,16 @@ const App = () => {
 
   return (
     <div>
+      filter shown with: <input value={filter} onChange={filterChangeHandler} />
       <Header />
-      <form onSubmit={submitHandler}>
-        <div>
-          name: <input value={newName} onChange={nameChangeHandler} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={numberChangeHandler} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name} {person.number}</p>
-      ))}
+      <ContactForm
+        submitHandler={submitHandler}
+        nameChangeHandle={nameChangeHandler}
+        numberChangeHandle={numberChangeHandler}
+        name={newName}
+        number={newNumber}
+      />
+      <Display persons={persons} filter={filter}/>
     </div>
   );
 };
